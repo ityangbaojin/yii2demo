665f5a868e7effbb5b3ee281a83f9eed53802b72
= <=>
2016-07-14 19:03:55 +0800

    check mime type...

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -13,4 +13,22 @@ class UserController extends Controller {
         $users = Users::find()->asArray()->all();
         return $this->render('index', compact('users'));
     }
+
+    // 检查文件mime type
+    public function actionTest() {
+        if (extension_loaded('fileinfo')) {
+            $encoding = false;
+            $fiObj = new \finfo($encoding ? FILEINFO_MIME : FILEINFO_MIME_TYPE);
+            $mimeType = $fiObj->file('./1.txt');
+            // $mimeType = $fiObj->buffer(file_get_contents('./1.txt'));
+            var_dump($mimeType); exit;
+        }
+
+        // 没fileinfon 扩展就用其它方式处理
+        $command = 'file -I "' . realpath('1.txt') . '"';
+        $shellOutput = trim(shell_exec($command));
+        $colonPosition = strpos($shellOutput, ':');
+        $shellGetMimeType = (substr($shellOutput, $colonPosition + 1));
+        var_dump(explode(';', $shellGetMimeType)[0]);
+    }
 }
